afd84b8eeb768870aa8fc68c0b663a7bd266ad67
cye2020 <ariaplay101@gmail.com>
2023-01-17 19:49:04 +0900

    LCS

---
+++ P0009251/solution1.py
@@ -0,0 +1,17 @@
+import sys
+
+
+input = sys.stdin.readline
+s1, s2 = input().strip(), input().strip()
+
+l1, l2 = len(s1), len(s2)
+cache = [0] * l2
+
+for i in range(l1):
+    cnt = 0
+    for j in range(l2):
+        if cnt < cache[j]:
+            cnt = cache[j]
+        elif s1[i] == s2[j]:
+            cache[j] = cnt + 1
+print(max(cache))
