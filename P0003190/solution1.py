9741b3116356b861fada9713920b51a3265d738f
cye2020 <ariaplay101@gmail.com>
2023-02-08 16:06:33 +0900

    solve: [뱀]

--- P0003190/solution1.py
+++ P0003190/solution1.py
@@ -2,54 +2,54 @@ import sys
 
 class Game:
     def __init__(self, N, L, field, rotates):
-        self.size = N
-        self.snake = Snake()
-        self.field = field
-        self.rotates = rotates
-        self.num = L
-        self.check = 0
-        self.turn = 0
+        self.size = N  # 보드의 크기
+        self.snake = Snake()  # 뱀
+        self.field = field  # 보드
+        self.rotates = rotates  # 방향 변환 정보
+        self.num = L  # 방향 전환 횟수
+        self.check = 0  # 방향 전환 횟수 기록
+        self.turn = 1  # 턴
     
     def __iter__(self):
         return self
     
     def __next__(self):
-        ny, nx = self.snake.move()
-        if (ny >= N or nx >= N or ny < 0 or nx < 0) or (field[ny][nx] == 2):
+        ny, nx = self.snake.move()  # 뱀 이동
+        if (ny >= N or nx >= N or ny < 0 or nx < 0) or (field[ny][nx] == 2):  # 게임 오버
             raise StopIteration
-        if field[ny][nx] == 0:
+        if field[ny][nx] == 0:  # 빈 공간
             oy, ox = self.snake.shorten()
             field[oy][ox] = 0
         field[ny][nx] = 2
-        self.turn += 1
-        if self.check < self.num and (self.rotates[self.check][0] == str(self.turn)):
+        if self.check < self.num and (self.rotates[self.check][0] == str(self.turn)):  # 회전 체크
             self.snake.rotate(self.rotates[self.check][1])
             self.check += 1
+        self.turn += 1
 
 
 class Snake:
     def __init__(self):
-        self.loc = [(0, 0)]
-        self.dx = [1, 0, -1, 0]
+        self.loc = [(0, 0)]  # 위치 기록
+        self.dx = [1, 0, -1, 0]  # 시계방향
         self.dy = [0, 1, 0, -1]
-        self.dir = 0
+        self.dir = 0  # 초기 진행 방향: 우측 (dx[0], dy[0])
         
-    def move(self):
+    def move(self):  # 진행
         nx = self.loc[-1][1] + self.dx[self.dir]
         ny = self.loc[-1][0] + self.dy[self.dir]
         self.loc.append((ny, nx))
         return ny, nx
     
-    def shorten(self):
+    def shorten(self):  # 몸 길이 줄임
         return self.loc.pop(0)
 
-    def right(self):
+    def right(self):  # 오른쪽 회전
         self.dir = (self.dir + 1) % 4
     
-    def left(self):
+    def left(self):  # 왼쪽 회전
         self.dir = (self.dir - 1) % 4
 
-    def rotate(self, r):
+    def rotate(self, r):  # 회전
         if r == 'D':
             self.right()
         elif r == 'L':
@@ -72,4 +72,4 @@ if __name__ == '__main__':
     for _ in game:
         pass
     
-    print(game.turn + 1)
+    print(game.turn)
